8c4f3b1107e2209afad028f4e56b3199309553ea
tessjaworski <tessjaworski@ufl.edu>
2026-04-06 22:05:59 -0400

    Added main

--- src/hvlcs.py
+++ src/hvlcs.py
@@ -1,3 +1,5 @@
+import sys
+
 def hvlcs(values, A, B):
     m = len(A)
     n = len(B)
@@ -12,8 +14,11 @@ def hvlcs(values, A, B):
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
     
+    # save the max value
     max_val = dp[m][n]
     ans = []
+
+    # get the subsequence
     while m > 0 and n > 0:
         if A[m-1] == B[n-1] and dp[m][n] == dp[m-1][n-1] + values[A[m-1]]:
             ans.append(A[m-1])
@@ -25,3 +30,22 @@ def hvlcs(values, A, B):
             n-=1
 
     return max_val, ''.join(reversed(ans))
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        lines = [line.strip() for line in f if line.strip()]
+
+    alphabet_size = int(lines[0])
+    values = {}
+    for i in range(1, alphabet_size + 1):
+        parts = lines[i].split()
+        char = parts[0]
+        val = int(parts[1])
+        values[char] = val
+
+    A,B = lines[alphabet_size+1], lines[alphabet_size+2]
+
+    max_value, subsequence = hvlcs(values,A,B)
+    print(max_value)
+    print(subsequence)
